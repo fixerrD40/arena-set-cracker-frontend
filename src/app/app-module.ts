2388c4eeff93ad7f15b66393c7dfbf96b3f67b73
fixerrD40 <stafford.hank@gmail.com>
2025-06-11 12:14:43 +0200

    Le Mans
    - Add navbar

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,16 +1,26 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Navbar } from './components/navbar/navbar';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Navbar
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
