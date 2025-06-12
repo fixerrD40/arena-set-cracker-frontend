6f7363faa5a9869cb44da3131384eb9e16f7760d
fixerrD40 <stafford.hank@gmail.com>
2025-06-12 11:14:07 +0200

    Le Mans
    - add pages content

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/pages/index/index').then(m => m.Index)
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./components/pages/about/about').then(m => m.About)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/pages/about/about.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [MatCardModule, MatIconModule, MatButtonModule],
+  templateUrl: './about.html',
+  styleUrls: ['./about.css', '../pages.css']
+})
+export class About {}
