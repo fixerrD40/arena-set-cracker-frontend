73a0c96f837d855ddf80ae22c259e00bb369d759
fixerrD40 <stafford.hank@gmail.com>
2025-06-17 21:45:05 +0200

    Pontorson
    - add auth capabilities

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { App } from './app/app';
 import { routes } from './app/routes';
 import { NgZone } from '@angular/core';
+import { TokenInterceptor } from './app/interceptors/token-interceptor';
 
 fetch('/assets/config.json')
   .then(response => response.json())
   .then(config => {
     bootstrapApplication(App, {
       providers: [
-        provideHttpClient(),
+        provideHttpClient(withInterceptorsFromDi()),
+        TokenInterceptor,
         provideRouter(routes),
         {
           provide: 'APP_CONFIG',
